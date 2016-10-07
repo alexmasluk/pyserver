315852e0a5f6bb60086ab17b0e8d1130042f7503
alexmasluk <alex.masluk@gmail.com>
2016-10-06 18:25:35 -0700

    remove python3 crap from proj

--- server.py
+++ server.py
@@ -1,41 +1,34 @@
+#!/usr/bin/python2
 #import socket module
 from socket import *
 serverSocket = socket(AF_INET, SOCK_STREAM)
 
 #Prepare a sever socket
-#Fill in start
 serverPort = 12000
 serverSocket.bind(('',serverPort))
+serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1) #from stackoverflow.com
 serverSocket.listen(1)
-#Fill in end
+
 while True:
     #Establish the connection
     print ('Ready to serve...')
-    connectionSocket, addr = serverSocket.accept() #Fill in start #Fill in end
+    connectionSocket, addr = serverSocket.accept() 
     try:
-        message = connectionSocket.recv(1024).decode()#Fill in start #Fill in end
+        message = connectionSocket.recv(1024).decode()
         print(message)
         filename = message.split()[1]
 
         with open(filename[1:]) as f:
             outputdata = f.read()
         #Send one HTTP header line into socket
-        #Fill in start
-        connectionSocket.send(b"HTTP/1.1 200 OK\n\n");
-        #Fill in end
+        connectionSocket.send("HTTP/1.1 200 OK\n\n");
         #Send the content of the requested file to the client
-        outputdata = bytes(outputdata, 'UTF-8')
-        print(outputdata)
         for i in range(0, len(outputdata)):
             connectionSocket.send(outputdata[i])
         connectionSocket.close()
     except IOError:
         #Send response message for file not found
-        #Fill in start
-        connectionSocket.send(b"HTTP/1.1 404 Not Found\n\n");
-        #Fill in end
+        connectionSocket.send("HTTP/1.1 404 Not Found\n\n");
         #Close client socket
-        #Fill in start
         connectionSocket.close()
-        #Fill in end
 serverSocket.close()
